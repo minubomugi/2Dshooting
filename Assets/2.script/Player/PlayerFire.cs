00e557650c4a18b2f4fb407965fd20160dc20f57
minubomugi <minubomugi@gmail.com>
2026-09-02 16:35:53 +0900

    feat 8 보조 총알 양쪽에 2개 발사하기 (보조 총알은 속도와 모양이 다름)
    아 어렵다~

--- Assets/2.script/Player/PlayerFire.cs
+++ Assets/2.script/Player/PlayerFire.cs
@@ -6,6 +6,7 @@ public class PlayerFire : MonoBehaviour
     // 필요 속성
     // 총알 프리팹
     public GameObject BulletPrefab;
+
     //생성위치
     public Transform LeftFirePointTransform;
     public Transform RightFirePointTransform;
@@ -13,6 +14,9 @@ public class PlayerFire : MonoBehaviour
     // 쿨타이머
     public float CoolTime = 0.5f;
     public float CoolTimer = 0;
+    
+    //- 오토 모드
+    public bool AutoFireMode = false;
 
     private void Start()
     {
@@ -21,11 +25,19 @@ public class PlayerFire : MonoBehaviour
 
     private void Update()
     {
+        // 오토 공격 모드 토글
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AutoFireMode = !AutoFireMode; // 토글
+        }
+        
+        
+        
         // 0.쿨타이머 감소
         CoolTimer -= Time.deltaTime;
         
-        // 1. 쿨타이머가 0초이하이고 같이 스페이스바 누르면
-        if (CoolTimer <= 0 && Input.GetKeyDown(KeyCode.Space))
+        // 1. 쿨타이머가 0초이하이고 같이 스페이스바 누르거나 오토 모드리면
+        if (CoolTimer <= 0 && (Input.GetKeyDown(KeyCode.Space)||AutoFireMode))
         {
             // 2. 발사
             Fire();
@@ -38,16 +50,13 @@ public class PlayerFire : MonoBehaviour
     //1. 스페이스바를 누르면
     private void Fire()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //2. 총알 프리팹을 생성한다.
-            //Instantiate는 프리팹으로부터 복사해서 게임 오브젝트를 만들고 씬에 넣어주는 기능
+        //2. 총알 프리팹을 생성한다.
+        //Instantiate는 프리팹으로부터 복사해서 게임 오브젝트를 만들고 씬에 넣어주는 기능
             
-            //5. 실습과제 발사하는 곳 2개 만들기
-            GameObject leftBullet = Instantiate(BulletPrefab);
-            leftBullet.transform.position = LeftFirePointTransform.position;
-            GameObject rightBullet = Instantiate(BulletPrefab);
-            rightBullet.transform.position = RightFirePointTransform.position;
-        }
+        //5. 실습과제 발사하는 곳 2개 만들기
+        GameObject leftBullet = Instantiate(BulletPrefab);
+        leftBullet.transform.position = LeftFirePointTransform.position;
+        GameObject rightBullet = Instantiate(BulletPrefab);
+        rightBullet.transform.position = RightFirePointTransform.position;
     }
 }
