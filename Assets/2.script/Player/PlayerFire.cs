2944dbfcbbb437c7dd501224f9c5d82343e42c2d
minubomugi <minubomugi@gmail.com>
2026-09-02 16:03:05 +0900

    feat. 실습 과제5 총알 양쪽 발사

--- Assets/2.script/Player/PlayerFire.cs
+++ Assets/2.script/Player/PlayerFire.cs
@@ -7,7 +7,9 @@ public class PlayerFire : MonoBehaviour
     // 총알 프리팹
     public GameObject BulletPrefab;
     //생성위치
-    public Transform firePoint;
+    public Transform LeftFirePointTransform;
+    public Transform RightFirePointTransform;
+    
     private void Update()
     {
         //1. 스페이스바를 누르면
@@ -15,8 +17,12 @@ public class PlayerFire : MonoBehaviour
         {
             //2. 총알 프리팹을 생성한다.
             //Instantiate는 프리팹으로부터 복사해서 게임 오브젝트를 만들고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = firePoint.position;
+            
+            //5. 실습과제 발사하는 곳 2개 만들기
+            GameObject leftBullet = Instantiate(BulletPrefab);
+            leftBullet.transform.position = LeftFirePointTransform.position;
+            GameObject rightBullet = Instantiate(BulletPrefab);
+            rightBullet.transform.position = RightFirePointTransform.position;
         }
     }
 }
