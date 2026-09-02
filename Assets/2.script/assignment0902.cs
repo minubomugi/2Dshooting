70f9c547b6f142c777e59a307ae21107375948d6
minubomugi <minubomugi@gmail.com>
2026-09-02 13:55:53 +0900

    fix 리팩토링해서 SpeedChage, Move 추출

--- Assets/2.script/assignment0902.cs
+++ Assets/2.script/assignment0902.cs
@@ -5,7 +5,27 @@ public class assignmnet0902 : MonoBehaviour
     public float Speed;
     public float limit;
 
-    private void Update()
+    private void Update() //객체의 이벤트래
+    {
+        Move();
+        SpeedChange(); // 콘텐츠 중심 명명법으로 짓는게 편해
+        //InputSpeedKey() -> 옛날에 명령받아서 진행하는 입력받아 하는 기능중심 명명법
+    }
+
+    private void SpeedChange()
+    {
+        //실습 과제 3 e버튼 누르면 스피드 업, q버튼누르면 스피드 1다운
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Speed++;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Speed--;
+        }
+    }
+
+    private void Move()
     {
         //똑같이 이동할 수 있는 거 처리
         float h = Input.GetAxis("Horizontal");
@@ -47,19 +67,9 @@ public class assignmnet0902 : MonoBehaviour
         if (transform.position.x > limit)
         {
             transform.position = new Vector3(-limit, transform.position.y, 0);
+                    
+            //이동
+            transform.Translate(nomalizedDirection * (Speed * Time.deltaTime));
         }
-        
-        //실습 과제 3 e버튼 누르면 스피드 업, q버튼누르면 스피드 1다운
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Speed++;
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            Speed--;
-        }
-        
-        //이동
-        transform.Translate(nomalizedDirection * (Speed * Time.deltaTime));
     }
 }
