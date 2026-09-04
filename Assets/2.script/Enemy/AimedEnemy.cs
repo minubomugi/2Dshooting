551918557ca84fc072f2c7b24a631408b7d8f731
minubomugi <minubomugi@gmail.com>
2026-09-04 10:43:11 +0900

    refactor: 코드 캐싱 이용

--- Assets/2.script/Enemy/AimedEnemy.cs
+++ Assets/2.script/Enemy/AimedEnemy.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class AimedEnemy : Enemy
 {
-    // 09/03 수업 과제 3-2
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     protected override void Move()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Vector2 direction = new Vector2(0, player.transform.position.y - transform.position.y).normalized;
+        if (_player == null) return;
+
+        Vector2 direction = new Vector2(0, _player.transform.position.y - transform.position.y).normalized;
         transform.Translate(direction * (_movespeed * Time.deltaTime));
     }
 }
