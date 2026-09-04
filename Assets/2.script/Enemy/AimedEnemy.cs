39cdcbf9cdfb4e12506f91d267fed75581b7b818
minubomugi <minubomugi@gmail.com>
2026-09-04 10:24:27 +0900

    docs: 제미나이 코드 리뷰 시스템 워크 플로우

--- Assets/2.script/Enemy/AimedEnemy.cs
+++ Assets/2.script/Enemy/AimedEnemy.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class AimedEnemy : Enemy
 {
-    private GameObject player;
-
     // 09/03 수업 과제 3-2
     protected override void Move()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector2 direction = new Vector2(0, player.transform.position.y - transform.position.y).normalized;
         transform.Translate(direction * (_movespeed * Time.deltaTime));
     }
--- Assets/2.script/Enemy/DownawadEnemy.cs
+++ Assets/2.script/Enemy/DownawadEnemy.cs
@@ -5,7 +5,7 @@ public class DownawadEnemy : Enemy
     // 09/03 수업 과제 3-1
     protected override void Move()
     {
-        Vector2 direction = Vector2.up;
+        Vector2 direction = Vector2.down;
         transform.Translate(direction * (_movespeed * Time.deltaTime));
     }
 }
