ca52e45870cd88363486b3abd9bdcd0f9a2f2812
minubomugi <minubomugi@gmail.com>
2026-09-10 13:16:06 +0900

    feat: 정수 UI와 ScoreManager
    캔버스를 제작하고 UGUI의 TEXT를 작성하여
     최고 점수와 현재 점수를 기록하는 매니저를 제작

---
+++ Assets/2.script/ScoreManager.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    // 관리: 특정 데이터에 대한 무결성과 생성, 읽기, 수정, 삭제 등과 관련된 로직
+    private int _bestScore;
+    private int _currentScore;
+
+    // UI 책임 추가(텍스트에서 프로 참조)
+    [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
+    [SerializeField] private TextMeshProUGUI _currentScoreTextUI;
+
+    private void Update()
+    {
+        _bestScoreTextUI.text = $"Best Score: {_bestScore}";
+        _currentScoreTextUI.text = $"Current Score: {_currentScore}";
+    }
+}
